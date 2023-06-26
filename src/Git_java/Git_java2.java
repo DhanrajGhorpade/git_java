62838984418508f518608a2bafc0109f10556ceb
Dhanraj Ghorpade <ghorpadedhanraj.v@gmail.com>
2023-06-26 18:14:46 +0530

    done

---
+++ src/Git_java/Git_java2.java
@@ -0,0 +1,8 @@
+package Git_java;
+
+public class Git_java2 {
+	Git_java2()
+	{
+		System.out.println("Successfully created");
+	}
+}
--- src/Git_java/Java_git.java
+++ src/Git_java/Java_git.java
@@ -4,6 +4,8 @@ public class Java_git {
 
 	public static void main(String[] args) {
 		System.out.println("JAVA GIT CONNECTIVITY");
+		Git_java2 obj = new Git_java2();
+		
 
 	}
 
